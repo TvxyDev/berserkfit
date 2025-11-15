6909ea72848bec66d9b7b984268b80b73d1636d5
Victor <94146374+TvxyDev@users.noreply.github.com>
2025-11-15 23:27:55 +0000

    Add port parameter to MySQL connection

--- ligacao.php
+++ ligacao.php
@@ -1,16 +1,14 @@
 <?php
-$servername = 'sql7.freesqldatabase.com';
-$username = 'sql7808008';
-$password = 'FVYW7uwPyH';
-$dbname = 'sql7808008';
+// Credenciais do FreeSQLDatabase.com
+$servername = "sql7.freesqldatabase.com";
+$username = "sql7808008";
+$password = "FVYW7uwPyH";
+$dbname = "sql7808008";
+$port = 3306;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
 if ($conn->connect_error) {
     die("Falhou a ligação: " . $conn->connect_error);
 }
-//echo "Ligação bem sucedida";
-
 ?>
-
-
-
